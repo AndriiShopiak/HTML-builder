e441807321c97eee16459a9f4541bdabebec6bd0
Andrii <andriyshopyak@gmail.com>
2023-05-04 13:19:37 +0300

    feat: add one more solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
- let stream = fs.readFileSync("01-read-file/text.txt", "utf-8");
- console.log(stream);
+// add reading file and implemet auto code utf-8
+let stream = new fs.ReadStream('01-read-file/text.txt', {encoding: 'utf-8'});
+ 
+stream.on('readable', function(){
+  let data = stream.read();
+  console.log(data);
+});
+//  Use end when file reading ended
+stream.on('end', function(){
+  console.log('THE END');
+});
  
