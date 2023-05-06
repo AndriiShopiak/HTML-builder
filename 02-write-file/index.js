db8cf03f4f4778ba1a824d8cd4c080d51de94229
Andrii Shopiak <andriyshopyak@gmail.com>
2023-05-06 12:13:00 +0300

    fix: remove test solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,28 +1,7 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
-// const { stdin, stdout } = process;
-// const url = path.join(__dirname, 'text.txt');
 
-// fs.writeFile(url, '', (err) => {
-//   if (err) throw err;
-//   console.log('Хай, как ты сюда забрёл друг? Ну-ка расскажи мне в консоли...');
-// });
-
-// process.on('exit', () => {
-//   stdout.write('Да? Очень интересно, ладно, всё удачи, мне пора!');
-//   process.exit();
-// });
-
-// stdin.on('data', data => fs.appendFile(url, data, err => {
-//   if (err) throw err;
-//   if (data.toString().trim() === 'exit') process.exit();
-// }));
-
-// process.on('SIGINT', () => {
-//   console.log('');
-//   process.exit()
-// });
 const url = path.join(__dirname, 'text.txt');
 process.on("exit" , () => {
   process.stdout.write("Wow, that's really cool, you're on your way, then good luck!");
