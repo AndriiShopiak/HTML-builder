0e7e167bac36fbba1c11f8a37dd1cd150a9e8d7e
Andrii Shopiak <andriyshopyak@gmail.com>
2023-05-07 12:22:28 +0300

    feat: add one more solution for task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,28 +4,22 @@ const { readdir, stat } = require('fs/promises');
 
 const directory = path.join(__dirname, 'secret-folder');
 
-// async function readDirectory(directoryPath) {
-//   const files = await readdir(directoryPath, { withFileTypes: true });
-//   for (const file of files) {
-//     if (file.isFile()) {
-//       const fileName = path.basename(file.name, path.extname(file.name));
-//       const fileExtname = path.extname(file.name).slice(1);
-//       const stats = await stat(path.join(directoryPath, file.name));
-//       const fileSize = parseFloat((stats.size / Math.pow(1024, 1)).toFixed(2));
 
-//       stdout.write(`\n${fileName} - ${fileExtname} - ${fileSize}kb`);
-//     }
-//   }
-// }
-
-// readDirectory(directory);
-
-async function readFileDir () {
+async function readFileDir (directoryPath) {
   try {
-    const files = await readdir(directory);
-    for (const file of files) console.log(file);
+    const files = await readdir(directoryPath, { withFileTypes: true });
+    for (const file of files) {
+      if (file.isFile()) {
+        const fileName = path.basename(file.name, path.extname(file.name));
+        const fileExtname = path.extname(file.name).slice(1);
+        const stats = await stat(path.join(directoryPath, file.name));
+        const fileSize = parseFloat((stats.size / Math.pow(1024, 1)).toFixed(2));
+        
+        stdout.write(`\n${fileName} - ${fileExtname} - ${fileSize}kb`);
+      }
+    }
   } catch (err) {
     console.error(err);
 }
 }
-readFileDir();
+readFileDir(directory);
