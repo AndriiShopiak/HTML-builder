5b3a8c2cff90a2b09cc5f1cf010f86f15f135376
Andrii Shopiak <andriyshopyak@gmail.com>
2023-05-08 14:52:30 +0300

    feat: add test solution for merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,3 +4,20 @@ import { EOL } from 'node:os';
 import { resolve, dirname, extname } from 'node:path';
 import { createWriteStream } from 'node:fs';
 import { readdir } from 'node:fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const bundleFilePath = resolve(__dirname, 'project-dist/bundle.css');
+const stylesDirPath = resolve(__dirname, 'styles');
+
+const stylesDirData = await readdir(stylesDirPath, { withFileTypes: true });
+const cssFiles = stylesDirData.filter((item) => item.isFile() && extname(item.name) === '.css');
+
+const ws = createWriteStream(bundleFilePath, 'utf-8');
+
+for (const file of cssFiles) {
+  const filePath = resolve(stylesDirPath, file.name);
+  const fileData = await readFile(filePath, 'utf-8');
+
+  ws.write(fileData + EOL);
+}
