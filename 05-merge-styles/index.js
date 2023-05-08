d11844cea998fde998a3b20f0e7b1505bf797ce4
Andrii Shopiak <andriyshopyak@gmail.com>
2023-05-08 19:11:01 +0300

    fix: fixed functions name and variabels

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const sourceFolder = 'styles';
+const srcFolder = 'styles';
 const outputFolder = 'project-dist';
 const fileName = 'bundle.css';
 
@@ -9,13 +9,13 @@ const outputFile = fs.createWriteStream(
   path.join(__dirname, outputFolder, fileName)
 );
 
-const buildBundle = async () => {
-  const files = await fs.promises.readdir(path.join(__dirname, sourceFolder), {
+async function buildBundle () {
+  const files = await fs.promises.readdir(path.join(__dirname, srcFolder), {
     withFileTypes: true,
   });
 
   for await (const file of files) {
-    const filePath = path.join(__dirname, sourceFolder, file.name);
+    const filePath = path.join(__dirname, srcFolder, file.name);
     if (file.isFile() && path.extname(file.name) === '.css') {
       fs.createReadStream(filePath).pipe(outputFile);
     }
