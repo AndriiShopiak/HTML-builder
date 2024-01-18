a8b1387524e333017248d4790c24187337cc0238
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-18 13:24:20 +0200

    refactor: edit code function

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,18 +1,17 @@
 const process = require('process');
 const fs = require('fs');
-const path = require('path');
-
-const url = path.join(__dirname, 'text.txt');
-fs.writeFile(url, '', () => { });
-process.on('exit', () => {
-  process.stdout.write('Wow, that\'s really cool, you\'re on your way, then good luck!');
-  process.exit();
-});
-console.log('Hey, how did you get here friend? Well, tell me in the console...');
-process.stdin.on('data', data => fs.appendFile(url, data, err => {
+fs.writeFile('02-write-file/text.txt', '', () => { });
+process.on('exit', stopProcess);
+process.stdin.on('data', data => fs.appendFile('02-write-file/text.txt', data, err => {
   if (err) throw err;
   if (data.toString().trim() === 'exit') process.exit();
 }));
 process.on('SIGINT', () => {
   process.exit();
 });
+console.log('Hey, how did you get here friend? Well, tell me in the console...');
+
+function stopProcess() {
+  process.stdout.write('Wow, that\'s really cool, you\'re on your way, then good luck!');
+  process.exit();
+}
