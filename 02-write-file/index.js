7e380ffc3fd5c8707b83116bc8f6d5dcc7e8c46d
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-18 09:40:46 +0200

    reafctor: edit code according code starndart

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 const url = path.join(__dirname, 'text.txt');
-fs.writeFile(url, "", (err) => {});
-process.on("exit" , () => {
-  process.stdout.write("Wow, that's really cool, you're on your way, then good luck!");
+fs.writeFile(url, '', () => { });
+process.on('exit', () => {
+  process.stdout.write('Wow, that\'s really cool, you\'re on your way, then good luck!');
   process.exit();
 });
-console.log("Hey, how did you get here friend? Well, tell me in the console...");
+console.log('Hey, how did you get here friend? Well, tell me in the console...');
 process.stdin.on('data', data => fs.appendFile(url, data, err => {
-    if (err) throw err;
-    if (data.toString().trim() === 'exit') process.exit();
-  }));
-process.on("SIGINT", () => {
+  if (err) throw err;
+  if (data.toString().trim() === 'exit') process.exit();
+}));
+process.on('SIGINT', () => {
   process.exit();
 });
