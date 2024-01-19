87ee74f32f72f7adf252ccc1048937d06c4b749b
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-19 15:26:06 +0200

    feat: add while loof for looping trought the files object

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,16 +8,19 @@ const directory = path.join(__dirname, 'secret-folder');
 async function readFileDir (directoryPath) {
   try {
     const files = await readdir(directoryPath, { withFileTypes: true });
-    for (const file of files) {
-      if (file.isFile()) {
-        const fileName = path.basename(file.name, path.extname(file.name));
-        const fileExtname = path.extname(file.name).slice(1);
-        const stats = await stat(path.join(directoryPath, file.name));
+    let counter = 0;
+
+    while (counter < files.length) {
+      if (files[counter].isFile()) {
+        const fileName = path.basename(files[counter].name, path.extname(files[counter].name));
+        const fileExtname = path.extname(files[counter].name).slice(1);
+        const stats = await stat(path.join(directoryPath, files[counter].name));
         const fileSize = parseFloat((stats.size / Math.pow(1024, 1)).toFixed(2));
-        
         stdout.write(`\n${fileName} - ${fileExtname} - ${fileSize}kb`);
       }
+      counter++;
     }
+    
   } catch (err) {
     console.error(err);
   }
