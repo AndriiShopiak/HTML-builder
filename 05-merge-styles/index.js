b7c1fbd9331ee9b774948f6188f5874a22ba48af
Andrii <andriyshopyak@gmail.com>
2024-01-20 13:42:40 +0200

    feat: add solution for createwriteStream

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,25 @@
+// Import all required modules
 const path = require('path');
 const fs = require('fs');
-
-const srcFolder = 'styles';
+// Define path for files
+const desFolder = 'styles';
 const exitFolder = 'project-dist';
 const FileName = 'bundle.css';
+const pathWritingFiles = path.join(__dirname, exitFolder, FileName);
 
-const outputFile = fs.createWriteStream(
-  path.join(__dirname, exitFolder, FileName)
-);
+const outputFile = fs.createWriteStream(pathWritingFiles);
 
 async function buildBundle () {
-  const files = await fs.promises.readdir(path.join(__dirname, srcFolder), {
+  const files = await fs.promises.readdir(path.join(__dirname, desFolder), {
     withFileTypes: true,
   });
 
   for await (const file of files) {
-    const filePath = path.join(__dirname, srcFolder, file.name);
+    const filePath = path.join(__dirname, desFolder, file.name);
     if (file.isFile() && path.extname(file.name) === '.css') {
       fs.createReadStream(filePath).pipe(outputFile);
     }
   }
-};
+}
 
 buildBundle();
