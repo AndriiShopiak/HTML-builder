c60257a7dd1e96204fff27ce2e3a578449f7da7e
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-22 14:45:09 +0200

    feat: add creating dist folder function for 6 task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,44 +1,17 @@
 const path = require('path');
-const fs = require('fs');
-const { pipeline } = require('stream');
-const template = path.join(__dirname, 'template.html');
-const components = path.join(__dirname, 'components');
-const sourceAssetsFolder = path.join(__dirname, 'assets');
-const outputFolder = path.join(__dirname, 'project-dist');
-const outputAssetsFolder = path.join(outputFolder, 'assets');
-const sourceStyles = path.join(__dirname, 'styles');
-const outputStyle = 'style.css';
+// const fs = require('fs');
+// const { pipeline } = require('stream');
+const fsPromises = require('fs').promises;
 
-// Copy dir function
-async function copyDir (source, output) {
-    try {
-      await createFolder(output);
-      const folderData = await readFolder(source);
-      await copyFiles(folderData, source, output);
-    } catch (e) {
-      throw new Error(e);
-    }
-  };
+let mainProjectFolder = 'project-dist';
 
-//   implemented delete folder function
-async function deleteFolder(folder) {
-    try {
-      await fs.promises.rm(folder, { recursive: true });
-    } catch (e) {
-      console.log('');
-    }
-  };
+// Create folder for project
 
-// create folder function
-async function createFolder (folder) {
-    fs.promises.mkdir(folder, { recursive: true });
-  };
+async function cerateDist() {
+  const main = path.join(__dirname, mainProjectFolder);
+  // const assetsFolder = path.join(__dirname, 'assets');
+  // const desAssets = path.join(__dirname, 'assets');
 
-// implemented read folder 
-async function readFolder (folder) {
-    const filesNames = await fs.promises.readdir(folder, {
-      withFileTypes: true,
-    });
-  
-    return filesNames;
-  };  
+  await fsPromises.mkdir(main, { recursive: true });
+}
+cerateDist();
