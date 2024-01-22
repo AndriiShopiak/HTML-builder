b0a2fdc5c7b3006778dd3cd10bbd8e5e7f2a597a
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-22 17:33:52 +0200

    feat: add message for last task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,17 +1,5 @@
-const path = require('path');
-// const fs = require('fs');
-// const { pipeline } = require('stream');
-const fsPromises = require('fs').promises;
 
-let mainProjectFolder = 'project-dist';
-
-// Create folder for project
-
-async function cerateDist() {
-  const main = path.join(__dirname, mainProjectFolder);
-  // const assetsFolder = path.join(__dirname, 'assets');
-  // const desAssets = path.join(__dirname, 'assets');
-
-  await fsPromises.mkdir(main, { recursive: true });
+function notImplemented() {
+  console.log('This task is not implemented');
 }
-cerateDist();
+notImplemented();
